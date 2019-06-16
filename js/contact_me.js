6247521c114fbcbd19bfbe1842127969a384893f
kim125kim125kim <47765654+kim125kim125kim@users.noreply.github.com>
2019-06-17 01:44:08 +0800

    改了很多

--- js/contact_me.js
+++ js/contact_me.js
@@ -20,7 +20,7 @@ $(function() {
       $this = $("#sendMessageButton");
       $this.prop("disabled", true); // Disable submit button until AJAX call is complete to prevent duplicate messages
       $.ajax({
-        url: "././kim125kim125kim@gmail.com/contact_me.php",
+        url: "././kim125kim125kim@outlook.com/contact_me.php",
         type: "POST",
         data: {
           name: name,
